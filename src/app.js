7d897287c02aaa32cf86057e6e50db66ed6e8501
Angel Gallego <angelgb1990@gmail.com>
2024-10-16 18:21:27 +0000

    realizados ultimos arreglos

--- src/app.js
+++ src/app.js
@@ -15,13 +15,15 @@ window.onload = function() {
   const genButton = document.getElementById("boton");
 
   let palos = ["♦", "♥", "♠", "♣"];
-  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
 
   function genCarta() {
     let palo = palos[Math.floor(Math.random() * palos.length)];
     let numero = numeros[Math.floor(Math.random() * numeros.length)];
-
-    let color = palo === "♥" || palo === "♦" ? "red" : "black";
+    let color =
+      palo === "♥" || palo === "♦"
+        ? "rgb(160, 10, 10)"
+        : "rgba(15, 13, 13, 0.87)";
 
     let colorPalo = `<p style="color: ${color};">${palo}</p>`;
     let colorNumero = `<p style="color: ${color};">${numero}</p>`;
@@ -32,13 +34,16 @@ window.onload = function() {
   }
   function TamañoCarta(tamaño) {
     if (tamaño.key === "Enter") {
-      let width = imputWidth.value || 400;
-      let height = imputheight.value || 600;
+      let width =
+        imputWidth.value == "" || imputWidth.value < 150
+          ? 385
+          : imputWidth.value;
+      let height =
+        imputheight.value == "" || imputheight.value < 200
+          ? 535
+          : imputheight.value;
       carta.style.width = `${width}px`;
       carta.style.height = `${height}px`;
-
-      // let fontSize = height * 0.15;
-      // carta.style.fontSize = `${fontSize}px`;
     }
   }
   imputWidth.addEventListener("keydown", TamañoCarta);
